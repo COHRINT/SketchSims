859f4ccb36480c037e97196958e5e9cbb7b9f1c9
Luke <clburks9@gmail.com>
2020-05-14 15:02:41 -0600

    misc

--- src/dataCheck.py
+++ src/dataCheck.py
@@ -1,7 +1,27 @@
 import numpy as np; 
 
 
-a = np.load('../data/simHARPS_Test.npy').item(); 
+#a = np.load('../data/simHARPS_Test.npy').item(); 
 
-print(a['tag'])
+#print(a['tag'])
+
+def check():
+
+    hum = np.load("../../../../../mnt/d/SimHARPS/simHARPS_Human_nom.npy").item(); 
+    #hum = np.load("../../../../../mnt/d/SimHARPS/simHARPS_Nonhuman_nom.npy").item(); 
+
+    allTimes = []; 
+    allCatchTimes = []; 
+
+    for sim in hum['sims']:
+        allTimes.append(sim['TotalTime']); 
+        if(sim['TotalTime'] < 300):
+            allCatchTimes.append(sim['TotalTime']); 
+
+    print("Average Time: {}".format(np.mean(allTimes)))
+    print("Average Catch Time: {}".format(np.mean(allCatchTimes))); 
+    print("Ratio of Capture: {}".format(len(allCatchTimes)/len(allTimes)));
+
+if __name__ == '__main__':
+    check(); 
 
